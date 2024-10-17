183bf059a4a6233f017e41038891837e5527c760
adg77 <98551497+adg77@users.noreply.github.com>
2024-10-17 13:41:06 -0500

    Update data.py

--- proprietary-perspectival-dataset-creation/data.py
+++ proprietary-perspectival-dataset-creation/data.py
@@ -20,6 +20,10 @@ table.add_column("Rating", justify="right", style="blue")
 
 # Add example data to the table
 table.add_row("Sports", "9119", "248", "1554", "7.5")
+table.add_row("Skit", "165000", "8906", "156000", "6.5")
+table.add_row("Gambling", "3300000", "247000","50000", "8" )
+table.add_row("Training", "39400", "1856", "10456", "6" )
+table.add_row("Materially Oriented Content","310000","1896","1100000","6")
 
 
 # Print the table to the console
